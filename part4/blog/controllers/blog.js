a3a0b25044d189bd542b9d377dde134244fe83ff
dominikehr <56166874+dominikehr@users.noreply.github.com>
2021-01-07 16:17:59 +0100

    exercise 4.13-4.14

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { result } = require('lodash')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response, next) => {
@@ -10,6 +11,20 @@ blogsRouter.get('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.get('/:id', async (request, response, next) => {
+  const { id } = request.params
+  try {
+    const blog = await Blog.findById(id)
+    if(blog) {
+      response.json(blog.toJSON())
+    } else {
+      response.status(404).end
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 blogsRouter.post('/', async (request, response, next) => {
   try {
     request.body.likes = request.body.likes || 0
@@ -18,6 +33,32 @@ blogsRouter.post('/', async (request, response, next) => {
     response.status(201).json(result)
   } catch (err) {
     next(err)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const body = request.body
+    const id = request.params.id
+    // {new: true} changes the default return value as to return the blog after(!) the update
+    const updatedBlog = await Blog.findByIdAndUpdate(id, body, {new: true})
+    if(updatedBlog) {
+      response.status(201).json(updatedBlog)
+    } else {
+      response.status(404).end
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
   }
 })
 
--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -46,39 +46,83 @@ beforeEach(async () => {
   await Promise.all(promiseArr)
 })
 
-// exercise 4.8
-test('blogs are returned as valid number of jsons', async () => {
-  const blogs = await api.get('/api/blogs')
-  expect(blogs.body).toHaveLength(initialBlogs.length)
+
+describe('tests for already existing blogs', () => {
+  // exercise 4.8
+  test('blogs are returned as valid number of jsons', async () => {
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body).toHaveLength(initialBlogs.length)
+  })
+  
+  // exercise 4.9
+  test('blogs have unique property "id" ', async () => {
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body[0].id).toBeDefined()
+  })
 })
 
-// exercise 4.9
-test('blogs have unique property "id" ', async () => {
-  const blogs = await api.get('/api/blogs')
-  expect(blogs.body[0].id).toBeDefined()
-})
 
+
+describe('addition of new blogs', () => {
 // exercise 4.10
-test('a blog can be added', async () => {
-  await api.post('/api/blogs').send(newBlog).expect(201)
+  test('a blog can be added via POST', async () => {
+    await api.post('/api/blogs').send(newBlog).expect(201)
   
-  const blogs = await api.get('/api/blogs')
-  expect(blogs.body).toHaveLength(initialBlogs.length + 1)
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body).toHaveLength(initialBlogs.length + 1)
   
-  const titles = blogs.body.map((blog) => blog.title)
-  expect(titles).toContain('Neu')
+    const titles = blogs.body.map((blog) => blog.title)
+    expect(titles).toContain('Neu')
+  })
+
+  // exercise 4.11 
+  test('a blog without a specified likes property should default to 0 likes', async () => {
+    await api.post('/api/blogs').send(blogWithNoLikes).expect(201)
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body[blogs.body.length-1].likes).toBe(0)
+  })
+
+  // exercise 4.12
+  test('adding a blog without title or url is not possible', async () => {
+    await api.post('/api/blogs').send(blogNoTitleOrUrl).expect(400)
+  })
 })
 
-// exercise 4.11 
-test('a blog without a specified likes property should default to 0 likes', async () => {
-  await api.post('/api/blogs').send(blogWithNoLikes).expect(201)
-  const blogs = await api.get('/api/blogs')
-  expect(blogs.body[blogs.body.length-1].likes).toBe(0)
+// exercise 4.13
+describe('deletion of a blog', () => {
+  test('deletion of first blog is possible', async () => {
+    const blogsAtStart = await api.get('/api/blogs')
+    // pick first blog to delete
+    const blogToDelete = blogsAtStart.body[0]
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+    
+    const blogsAtEnd = await api.get('/api/blogs')
+    expect(blogsAtEnd.body).toHaveLength(initialBlogs.length - 1)
+    const contents = blogsAtEnd.body.map((blog) => blog.title)
+    expect(contents).not.toContain(blogToDelete.title)
+  })
 })
 
-// exercise 4.12
-test('adding a blog without title or url is not possible', async () => {
-  await api.post('/api/blogs').send(blogNoTitleOrUrl).expect(400)
+// exercise 4.14
+describe('update a blog', () => {
+  test('chage amount of likes on last blog', async () => {
+    const blogsAtStart = await api.get('/api/blogs')
+    // pick last blog to update
+    const last = blogsAtStart.body.length - 1
+    const blogToUpdate = blogsAtStart.body[last]
+    const dataToUpdate = {
+      likes: 10
+    }
+
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(dataToUpdate)
+      .expect(201)
+
+    const blogsAfterUpdate = await api.get('/api/blogs')
+    const contents = blogsAfterUpdate.body.map(blog => blog.likes)
+    expect(contents).toContain(updatedBlog.body.likes)
+  } )
 })
 
 afterAll(() => {
